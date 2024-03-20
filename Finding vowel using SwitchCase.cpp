cad63362562cc6e7338c1f3e603a36536418b08c
Adii <87767285+eddy0209@users.noreply.github.com>
2024-03-20 20:37:02 +0530

    Add files via upload

---
+++ Finding vowel using SwitchCase.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	char x;
+	printf("Enter the alphabet:");
+	scanf("%d",&x);
+	switch (x){
+		case a:
+			printf("The alphabet is vowel");
+		case e:
+			printf("The alphabet is vowel");
+		case i:
+			printf("The alphabet is vowel");
+		case o:
+			printf("The alphabet is vowel");
+		case u:
+			printf("The alphabet is vowel");
+			break;
+		default:
+			printf("The alphabet is consonant"):					
+		 
+	}
+	return 0;
+}
